fae71b3a14e388e23f735fddea47121eaf231b20
abrarzahed <abrarzahed6986@gmail.com>
2022-08-08 17:57:38 +0600

    completed 89th

--- app.js
+++ app.js
@@ -578,3 +578,21 @@ function myFunction(a, b) {
 }
 console.log(myFunction(new Date(Date.UTC(2000, 01, 01)), 31));
 */
+
+/*
+// Write a function that takes two date instances as argument
+// It should return true if the difference between the dates is less than or equal to 1 hour
+// It should return false otherwise
+function myFunction(a, b) {
+  const oneHour = 60 * 60 * 1000;
+  const dif = Math.abs(a.getTime() - b.getTime());
+  return dif <= oneHour;
+}
+console.log(
+  myFunction(new Date("2000/01/01 08:00:00"), new Date("2000/01/01 08:45:00"))
+);
+
+console.log(
+  myFunction(new Date("2000/01/01 08:00:00"), new Date("2000/01/01 09:45:00"))
+);
+*/
